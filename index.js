39ddf39be9e2d4f49229b75aef3419d9b4201010
rjbultitude <richard.bultitude@gmail.com>
2017-06-26 23:16:04 +0100

    update package fields with new name. update demo refs and build output name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //
-// Musical Scales index
+// Freqi index
 //
 // Copyright (c) 2017 Richard Bultitude. See LICENSE-MIT for further details.
 //
 // @see README.md for usage information
-module.exports = require('./lib/musicalscales');
+module.exports = require('./lib/freqi');
