789432624210c5348be36c79b1c4ad8a85c904eb
rjbultitude <richard.bultitude@gmail.com>
2019-01-19 17:02:46 +0000

    code formatting updates to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname)).listen(
-  8080, function(){
-console.log('Server running on 8080...');
-});
+  8080,
+  function() {
+    console.log('Server running on 8080...');
+  }
+);
