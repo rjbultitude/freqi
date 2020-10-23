76e540bf6b4ee17ab20203d44057d095eda25916
rjbultitude <richard.bultitude@gmail.com>
2020-10-23 10:45:52 +0100

    remove duplicate index file. add esm entry point field (module in pkg json) add new output to rollup config and create new mjs bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,17 @@
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-  input: 'src/freqi.ts',
+  input: './src/freqi.ts',
   plugins: [
     typescript()
   ],
-  output: {
-    file: 'lib/freqi.js',
+  output: [{
+    file: './lib/freqi.js',
     format: 'umd',
     name: 'freqi'
-  }
+  }, {
+    file: './lib/freqi.mjs',
+    format: 'es',
+    name: 'freqi'
+  }]
 };
